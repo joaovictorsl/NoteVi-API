a5629f64c565fb19b5cb053f70e10cecf236bccf
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-04-13 18:09:21 -0300

    Getting a note with authentication

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Note = require('../models/notes');
 const withAuth = require('../middlewares/auth');
+const { json } = require('express');
 
 router.post('/', withAuth, async (req, res) => {
   const { title, body } = req.body;
@@ -14,4 +15,27 @@ router.post('/', withAuth, async (req, res) => {
     res.status(500).json({ error: 'Error while creating a new note.' })
   }
 })
+
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const { id } = req.params;
+    let note = await Note.findById(id);
+    if (isOwner(req.user, note)) {
+      res.json(note)
+    } else {
+      res.status(403).json({ error: 'Access denied.' })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error while getting a note.' })
+  }
+})
+
+const isOwner = (user, note) => {
+  if (JSON.stringify(user._id) == JSON.stringify(note.author._id)) {
+    return true
+  } else {
+    return false
+  }
+}
+
 module.exports = router;
