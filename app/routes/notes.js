61103b1384a6abe4da9dc187498f285425f47b6e
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-04-13 20:22:15 -0300

    Delete, create, list and get a specific note are working

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -30,6 +30,46 @@ router.get('/:id', withAuth, async (req, res) => {
   }
 })
 
+router.get('/', withAuth, async (req, res) => {
+  try {
+    let notes = await Note.find({ author: req.user._id })
+    res.json(notes);
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+})
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    let note = await Note.findById(req.params.id);
+    if (isOwner(req.user, note)) {
+      note.title = req.body.title;
+      note.body = req.body.body;
+      note.updated_at = Date();
+      await Note.findByIdAndUpdate(req.params.id, note)
+      res.json(note).status(200)
+    } else {
+      res.status(403).json({ error: 'Access denied.' })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Problem to update a note' })
+  }
+})
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    let note = await Note.findById(req.params.id);
+    if (isOwner(req.user, note)) {
+      await note.delete()
+      res.json(note).status(204)
+    } else {
+      res.status(403).json({ error: 'Access denied.' })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Problem to delete a note' })
+  }
+})
+
 const isOwner = (user, note) => {
   if (JSON.stringify(user._id) == JSON.stringify(note.author._id)) {
     return true
