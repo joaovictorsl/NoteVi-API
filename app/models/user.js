59795050b7851b34a66e094bb6c55ea1daea7607
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-04-12 23:53:25 -0300

    Registering users

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let userSchema = new mongoose.Schema({
   name: String,
@@ -6,6 +7,19 @@ let userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   create_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
+})
+
+userSchema.pre('save', function (next) {
+  if (this.isNew || this.isModified('password')) {
+    bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+      if (err) {
+        next(err)
+      } else {
+        this.password = hashedPassword;
+        next();
+      }
+    })
+  }
 })
 
 module.exports = mongoose.model('User', userSchema)
