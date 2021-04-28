5201e90201c8c92a8cdecd51376cb87f44653ce5
João Victor <joaovictordev2003@gmail.com>
2021-04-28 12:15:19 -0300

    Deploy

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect('mongodb://localhost/notevi-node', {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
