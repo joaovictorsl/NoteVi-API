113888d6b6f4d66f370ab2bdc0f4075fb545b0fe
João Victor <joaovictordev2003@gmail.com>
2021-04-28 23:56:25 -0300

    Second deploy try

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 require('./config/database')
 var cors = require('cors')
+app.use(cors())
 
 var usersRouter = require('./app/routes/users');
 var notesRouter = require('./app/routes/notes');
@@ -13,7 +14,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
